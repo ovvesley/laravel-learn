c8324defcb30a1c7cddc48dc56d0bb2b82e2dc55
Wesley <ovvesley@protonmail.com>
2021-02-08 18:55:44 -0300

    Introdução: as Rotas, Controllers e Views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobrenos');
+
+Route::get('/contato', 'ContatoController@contato');
